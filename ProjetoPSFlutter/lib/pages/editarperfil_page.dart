60776bb484d31f93b2be8c3679af4f44ab8a9aea
Felipe <fs.cicatriz@hotmail.com>
2020-06-18 19:17:19 -0300

    Correção linhas roxas no editar perfil

--- ProjetoPSFlutter/lib/pages/editarperfil_page.dart
+++ ProjetoPSFlutter/lib/pages/editarperfil_page.dart
@@ -55,11 +55,9 @@ class _EditarPerfilPageState extends State<EditarPerfilPage> {
                   child: Column(
                     children: <Widget>[
                       Material(
-                        color: Colors.purple,
                         child: Container(
                             width: 350,
                             decoration: BoxDecoration(
-                                color: Colors.purple,
                                 borderRadius: BorderRadius.circular(10.0)),
                             child: Padding(
                                 padding: EdgeInsets.all(0),
